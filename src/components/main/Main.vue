247727d516d905789313401b6cb999508ad95d79
seanbrookes <seanbrookes@beachair.ca>
2023-07-23 08:04:47 -0700

    convert node to es6 clean up and add logging to inbox.php

--- src/components/main/Main.vue
+++ src/components/main/Main.vue
@@ -1,9 +1,18 @@
 <script setup>
 import { PostList } from '../post_list/';
 import { PostEditor } from '../post_editor/';
+
+let uxMode = '';
+const urlParams = new URLSearchParams(window.location.search);
+uxMode = urlParams.get('mode');
+
+const currentPost = urlParams.get('post');
+
+const isShowEditor = (uxMode === 'edit') && currentPost;
+
 </script>
 <template>
-  <header>Graffiti Engine</header>
-  <PostEditor />
+  <header><a href="/">Graffiti Engine</a></header>
+  <PostEditor v-if="isShowEditor" />
   <PostList />
 </template>
