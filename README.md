c5855a7dabf6526f507ce74f6ac6f33247cd6ff5
seanbrookes <seanbrookes@beachair.ca>
2023-07-29 09:18:13 -0700

    rough but loading doc - clearing doc - preview - autosave toggle

--- README.md
+++ README.md
@@ -1,5 +1,49 @@
 # Graffiti Engine 4
 
+## stack
+- [vite - build (Vue/js)](https://vitejs.dev/)
+- vue 3
+- [codemirror - code/markdown editor](https://codemirror.net/docs/guide/)
+- [marked - convert markdown to html](https://marked.js.org/)
+- [d3](https://d3js.org/)
+- [showdown - markdown to html converter](https://github.com/showdownjs/showdown)
+- [turndown - html to markdown](https://github.com/mixmark-io/turndown)
+- [underscore - html template compilation](https://underscorejs.org/#template)
+
+node v18
+
+frontend default port 5173
+```
+$npm run dev 
+```
+post server:  port 4444
+```
+$node server/server.js
+```
+
+## painting posts 
+This is resurected from previous versions of the engine so could use some revisting and updating
+- for now it is functional again to get a post compiled and posted to the target  
+   
+On the server side it uses showdown to generate markup from the post.body (markdown string)
+then underscore _.template to merge with base page template
+
+```
+  const converter = new showdown.Converter();
+  const templateData = fs.readFileSync('./server/templates/postTemplate.html', 'utf8');
+  var compiled = _.template(templateData);  
+  post.markup = converter.makeHtml(post.body);  // referenced in the template
+  const publishDoc = compiled(post);
+```
+
+Not part of the publish flow but needed as a utility sometimes
+HTML to markdown converter
+```
+const convertedBody = turndownService.turndown(postBody);
+```
+
+localhost test input location
+http://localhost/fourfivesix/inbox/inbox.php
 
 [marked](https://github.com/markedjs/marked)
 [Code mirror](https://codemirror.net/docs/)
--- src/components/post_editor/PostEditor.vue
+++ src/components/post_editor/PostEditor.vue
@@ -3,7 +3,7 @@ import { ref, inject, reactive, watch, computed, onMounted } from 'vue';
 import { EditorView, basicSetup } from 'codemirror';
 import { javascript } from '@codemirror/lang-javascript';
 import { markdown } from '@codemirror/lang-markdown';
-
+import {EditorState} from "@codemirror/state"
 import { marked } from 'marked';
 import { mangle } from "marked-mangle";
 import { gfmHeadingId } from "marked-gfm-heading-id";
@@ -16,34 +16,94 @@ const store = inject('store');
 
 let editor = {};
 let currentHTML = '';
+let currentView = 'editor';
+const isAutoSaveRef = ref();
+
+const setTabView = (event) => {
+  const value = event.target.value;
+  currentView = value;
+  renderCodeMirror();
+
+};
+
+let isAutoSave = ref(true);
+
 setTimeout(() => {
   console.log('| Timeout firing -- now --');
-  marked.use(mangle());
-  marked.use(gfmHeadingId(markedOptions));
-  const currentHTML = marked.parse(store?.state?.currentPost?.body);
-  document.getElementById('editor').innerHTML = currentHTML;
-
-  const code_mirror_el = document.getElementById('code_mirror_editor_el');
-  // let view = new EditorView({
-  //   doc: store?.state?.currentPost?.body,
-  //   extensions: [basicSetup, javascript()],
-  //   parent: el
-  // });
-  let view = new EditorView({
-    doc: store?.state?.currentPost?.body,
-    extensions: [basicSetup, markdown()],
-    parent: code_mirror_el
-  });  
+  // renderCodeMirror();
 
 }, 2000);
+const previewEl = ref(null);
+const editorEl = ref(null);
 
-// onMounted(() => {
-//   console.log('| onMounted firing -- now -- ',  store?.state?.currentPost?.body);
-//   if (store?.state?.currentPost?.body) {
-//     document.getElementById('editor').innerHTML = currentPostDom;
-//   }
-// });
+const renderCodeMirror = () => {
+  console.log('| renderCodeMirror currentView ', currentView);
+  /**
+   * 
+   *  MARKED EDITOR
+   * 
+   * 
+   */
 
+  if (currentView === 'preview') {
+    const editorEl = document.getElementById('editor');
+    marked.use(mangle());
+    marked.use(gfmHeadingId(markedOptions));
+    const currentHTML = marked.parse(store?.state?.currentPost?.body);    
+    previewEl.value.innerHTML = currentHTML;
+
+  }
+  else {
+  /**
+   * 
+   *  CODE MIRROR EDITOR
+   * 
+   * 
+   */
+   const code_mirror_el = document.getElementById('code_mirror_editor_el');
+   previewEl.value.innerHTML = null;
+    // let view = new EditorView({
+    //   doc: store?.state?.currentPost?.body,
+    //   extensions: [basicSetup, javascript()],
+    //   parent: el
+    // });
+    // editor.refresh();
+    const editorValue = editor.state.doc.toString();
+    editor.dispatch({
+          changes: {
+            from: 0,
+            to: editorValue.length,
+            insert: store?.state?.currentPost?.body || "",
+          },
+        })
+    // else {
+    //   console.warn('|  code mirror render called but no code mirror element');
+    // }
+
+  }
+
+};
+let sync_val = null;
+const editorState = EditorState.create({
+            doc: store?.state?.currentPost?.body,
+            extensions: [
+                EditorView.updateListener.of(function(e) {
+                    sync_val = e.state.doc.toString();
+                }),
+                basicSetup, markdown(),
+            ]
+        });
+
+onMounted(() => {
+  console.log('| onMounted firing -- now -- title? ',  store?.state?.currentPost?.title);
+  console.log('| onMounted firing -- currentView ',  currentView );
+  editor = new EditorView({
+        state: editorState,  
+        // doc: store?.state?.currentPost?.body,
+        // extensions: [basicSetup, markdown()],
+        parent: editorEl.value
+      });  
+});
 const currentPostDom = computed(() => {
   console.log("computed currentPostDom");
   let currentHTML = null;
@@ -59,16 +119,58 @@ const currentPost = computed(() => {
   console.log('| computed currentPost');
   return store?.state?.currentPost?.body;
 });
+watch(isAutoSave, (value) => {
+  console.log('|');
+  console.log('|  it is working isAutoSave ', isAutoSave.value);
+  console.log('|');
+});
+watch(currentPost, (value) => {
+  console.log('|');
+  // console.log('|  WE HAVE A BODY ', value);
+  renderCodeMirror();
+  console.log('|');
+});
+
+const toggleAutoSave = (event) => {
+  console.log('|');
+  console.log('| toggle auto save isAutoSaveRef.checked ', isAutoSaveRef.checked);
+  console.log('|');
+}
+
+
+
+const clearNew = () => {
+  if (confirm('clear this post?')) {
+    document.location.href = '/';
+  }
+};
+
 
 </script>
 <template>
   <div>Post Editor</div>
-  <div id="code_mirror_editor_el"></div>
+  <button @click="clearNew">clear</button>
+  <button value="editor" @click="setTabView">editor</button>
+  <button value="preview" @click="setTabView">preview</button>
 
-  <!-- Create the editor container -->
-  <div id="editor"></div>
-  <hr />
-  <div>{{ currentPostDom }}</div>
-  <hr />
-  <div>{{ currentPost }}</div>
+  <h1 v-if="currentView === 'editor'">CodeMirror</h1>
+  <h1 v-else>preview</h1>
+  <!-- marked preview -->
+  <div id="editor" ref="previewEl"></div>  
+  <div data-id="editor_view_container">
+    <!-- codemirror edting -->
+    <div id="code_mirror_editor_el" ref="editorEl"></div>
+
+  </div>
+  <div>
+    <button>save</button>
+    <label>autosave<input type="checkbox" ref="isAutoSaveRef" v-model="isAutoSave" /></label>
+  </div>
 </template>
+<style scoped>
+[data-id="editor_view_container"] {
+  border: 1px solid #eeeeee;
+  max-height: 500px;
+  overflow-y: scroll;
+}
+</style>
